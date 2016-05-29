c6faa73bf795d7ec1286fce17b42a9dbcebabdc3
Shem Hale <s.hale@aggiemail.usu.edu>
2016-05-29 13:56:32 -0600

    fix scss with react

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -7,14 +7,25 @@ export const MainLayout = ({content}) => (
   // className instead of class because class means something else in JS
   <div className="main-layout">
     <header>
-      <nav>
-        <h2>Shem Studios</h2>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-	  <li><a href="/studio">Studio</a></li>
-	  <li><a href="/contact">Contact</a></li>
-        </ul>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+	      <span className="icon-bar"></span>
+	      <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="/">Shem Studios</a>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+	      <li><a href="/studio">Studio</a></li>
+	      <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
     <main>
