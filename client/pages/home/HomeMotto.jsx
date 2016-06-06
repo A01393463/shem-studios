e81b19e7003f73a0c07f8dd5326dd8c5d929a606
Shem Hale <s.hale@aggiemail.usu.edu>
2016-06-05 21:08:21 -0600

    add svg mottos

--- client/pages/home/HomeMotto.jsx
+++ client/pages/home/HomeMotto.jsx
@@ -4,7 +4,7 @@ export default class HomeMotto extends Component {
   render() {
     return (
       <div className="motto col-md-4">
-        <img className="center-block" src={"/images/" + this.props.image} alt={this.props.alt} />
+        <img className={"center-block " + this.props.class} src={"/images/" + this.props.image} alt={this.props.alt} />
         <h1 className="text-center">{this.props.motto}</h1>
       </div>
     )
